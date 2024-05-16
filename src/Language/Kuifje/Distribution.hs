5e809c6e82ee31dea7cd2b411b53f8175b743fcd
V. Jackson <vjackson725@users.noreply.github.com>
2024-05-16 19:28:15 +1000

    add ProbList datatype + disambiguate monad syntax

--- src/Language/Kuifje/Distribution.hs
+++ src/Language/Kuifje/Distribution.hs
@@ -5,40 +5,49 @@
 
 module Language.Kuifje.Distribution where
 
-import Prelude hiding (filter, foldr, return, (>>=))
+import qualified Data.Map.Strict as M
+import Data.Bifunctor (first)
 import Data.List (genericLength)
-import Data.Map.Strict
+import Data.Map.Strict (Map)
 
 -- | Type synonym for probabilities.
 type Prob = Rational
+
+
+-- | List of values with probabilities.
+--   Probabilities should be in (0,1].
+--   Values may be duplicated.
+newtype ProbList a = ProbList { unProbList :: [(a,Prob)] }
+  deriving (Show)
+
+-- if f is not injective, values can become duplicated
+instance Functor ProbList where
+  fmap f = ProbList . fmap (first f) . unProbList
+
+instance Applicative ProbList where
+  pure x = ProbList [(x,1)]
+  fs <*> xs = ProbList [ (f x, p*q) | (x,q) <- unProbList xs, (f,p) <- unProbList fs ]
+
+instance Monad ProbList where
+  xs >>= f =
+    ProbList $
+      (do
+        (x,p) <- unProbList xs
+        (y,q) <- unProbList $ f x
+        return (y, p * q))
+
 
 -- | Distribution data type.
 newtype Dist a = D { runD :: Map a Prob }
   deriving (Show)
 
--- | Type alias for hyper-distributions.
-type Hyper a = Dist (Dist a)
+-- | Recover the map representation of a distribution, reduced.
+unpackD :: Dist a -> Map a Prob
+unpackD = M.filter (/= 0) . runD
 
--- | Top-level fmap function for distributions.
-fmap :: (Ord b) => (a -> b) -> Dist a -> Dist b
-fmap f dx = dx >>= (return . f)
-
--- | Top-level return function for distributions. Creates a singleton
--- distribution.
-return :: (Ord a) => a -> Dist a
-return x = D $ singleton x 1
-
--- | Alias for return function.
-point :: Ord a => a -> Dist a
-point = return
-
--- | Top-level bind function for distributions.
-(>>=) :: (Ord b) => Dist a -> (a -> Dist b) -> Dist b
-d >>= f = D $ fromListWith (+) [(y, p * q) | (x, p) <- toList $ runD d, (y, q) <- toList $ runD (f x)]
-
--- | Top-level join function for distributions.
-join :: (Ord a) => Hyper a -> Dist a
-join x = x >>= id
+-- | Remove zeroes from a distribution.
+reduction :: Dist a -> Dist a
+reduction = D . unpackD
 
 instance Ord a => Eq (Dist a) where
   d1 == d2  =  unpackD d1 == unpackD d2
@@ -46,26 +55,56 @@ instance Ord a => Eq (Dist a) where
 instance Ord a => Ord (Dist a) where
   d1 <= d2  =  unpackD d1 <= unpackD d2
 
+
+-- | Type alias for hyper-distributions.
+type Hyper a = Dist (Dist a)
+
+
+-- | Convert from Dist to ProbList
+distToProbList :: Dist a -> ProbList a
+distToProbList = ProbList . M.toList . runD
+
+-- | Convert from ProbList to Dist
+probListToDist :: Ord a => ProbList a -> Dist a
+probListToDist = D . M.fromListWith (+) . unProbList
+
+
+-- | fmap function for distributions.
+fmapDist :: (Ord b) => (a -> b) -> Dist a -> Dist b
+fmapDist f = probListToDist . fmap f . distToProbList
+
+-- | fmap function for distributions, where f is injective.
+--   Requires f is injective on all values in the domain of the Dist.
+fmapDistInj :: (Ord b) => (a -> b) -> Dist a -> Dist b
+fmapDistInj f = D . M.fromList . unProbList . fmap f . distToProbList
+
+-- | Top-level return function for distributions. Creates a singleton
+-- distribution.
+returnDist :: (Ord a) => a -> Dist a
+returnDist x = D $ M.singleton x 1
+
+-- | Alias for return function.
+point :: Ord a => a -> Dist a
+point = returnDist
+
+-- | Top-level bind function for distributions.
+bindDist :: (Ord b) => Dist a -> (a -> Dist b) -> Dist b
+bindDist d f = probListToDist $ (>>=) (distToProbList d) (distToProbList . f)
+
+-- | Top-level join function for distributions.
+joinDist :: (Ord a) => Hyper a -> Dist a
+joinDist x = bindDist x id
+
 -- | Construct a discrete distribution from a nonempty list of elements,
 -- assigning the same probability to each element.
 uniform :: (Ord a) => [a] -> Dist a
-uniform l = D $ fromListWith (+) [(x, 1 / genericLength l) | x <- l]
+uniform l = D $ M.fromListWith (+) [(x, 1 / genericLength l) | x <- l]
 
 -- | Construct a distribution in which the first element has probability p
 -- and the second 1−p.
 choose :: (Ord a) => Prob -> a -> a -> Dist a
-choose p x y = D $ fromListWith (+) [(x, p), (y, 1 - p)]
-
--- | Recover the map representation of a distribution, reduced.
-unpackD :: Dist a -> Map a Prob
-unpackD = removeZeroes . runD
-  where
-    removeZeroes = filter (/= 0)
-
--- | Remove zeroes from a distribution.
-reduction :: Dist a -> Dist a
-reduction = D . unpackD
+choose p x y = D $ M.fromListWith (+) [(x, p), (y, 1 - p)]
 
 -- | Sum the probabilities in the distribution.
 weight :: Dist a -> Prob
-weight (D l) = foldr (+) 0 l
+weight = M.foldr (+) 0 . runD
--- src/Language/Kuifje/Semantics.hs
+++ src/Language/Kuifje/Semantics.hs
@@ -15,16 +15,16 @@ type a ~~> b = Dist a -> Hyper b
 
 -- | Bind with reduction applied to the input distribution.
 (=>>) :: (Ord b) => Dist a -> (a -> Dist b) -> Dist b
-m =>> f = reduction m >>= f
+m =>> f = bindDist (reduction m) f
 
 -- | Kleisli composition.
 (==>) :: (Ord c) => (a ~> b) -> (b ~> c) -> (a ~> c)
-f ==> g = \x -> f x >>= g
+f ==> g = \x -> bindDist (f x) g
 
 -- | For a given program, returns a function that calculates the
 -- hyper-distribution for a given input distribution.
 hysem :: (Ord s) => Kuifje s -> (s ~~> s)
-hysem Skip          = return
+hysem Skip          = returnDist
 hysem (Update f p)  = huplift f ==> hysem p
 hysem (If c p q r)  = conditional c (hysem p) (hysem q) ==> hysem r
 hysem (While c p q) = let wh = conditional c (hysem p ==> wh) (hysem q)
@@ -34,20 +34,20 @@ hysem (Observe f p) = hobsem f ==> hysem p
 -- | Conditional semantics ('If' and 'While').
 conditional :: (Ord s) => (s ~> Bool) -> (s ~~> s) -> (s ~~> s) -> (s ~~> s)
 conditional c t e d
-  = let d' = d =>> \s -> c s =>> \b -> return (b, s)
+  = let d' = d =>> \s -> c s =>> \b -> returnDist (b, s)
         w1 = sum [p | ((b, _), p) <- toList $ runD d', b]
         w2 = 1 - w1
         d1 = D $ fromListWith (+) [(s, p / w1) | ((b, s), p) <- toList $ runD d', b]
         d2 = D $ fromListWith (+) [(s, p / w2) | ((b, s), p) <- toList $ runD d', not b]
         h1 = t d1
         h2 = e d2
-    in  if       null (runD d2)  then  h1
-        else if  null (runD d1)  then  h2
-                                 else  join (choose w1 h1 h2)
+    in  if      null (runD d2) then h1
+        else if null (runD d1) then h2
+                               else joinDist (choose w1 h1 h2)
 
 -- | Lifts a distribution to a hyper-distribution.
 huplift :: (Ord s) => (s ~> s) -> (s ~~> s)
-huplift f = return . (=>> f)
+huplift f = returnDist . (=>> f)
 
 -- | 'Observe' semantics.
 hobsem :: (Ord s, Ord o) => (s ~> o) -> (s ~~> s)
@@ -55,17 +55,17 @@ hobsem f = multiply . toPair . (=>> obsem f)
   where
 
     obsem :: (Ord o, Ord a) => (a ~> o) -> a ~> (o,a)
-    obsem f' x = fmap (\w -> (w, x)) (f' x)
+    obsem f' x = fmapDist (\w -> (w, x)) (f' x)
 
     toPair :: (Ord s, Ord o) => Dist (o, s) -> (Dist o, o -> Dist s)
     toPair dp = (d, f')
       where
-        d     = fmap fst dp
+        d     = fmapDist fst dp
         f' ws = let dpws = D $ fromListWith (+) [(s, p) | ((ws', s), p) <- toList $ runD dp, ws' == ws]
                 in D $ fromListWith (+) [(s, p / weight dpws) | (s, p) <- toList $ runD dpws]
 
     multiply :: (Ord s) => (Dist o, o -> Dist s) -> Hyper s
-    multiply (d, f') = fmap f' d
+    multiply (d, f') = fmapDist f' d
 
 -- | Calculate Bayes Vulnerability for a distribution.
 bayesVuln :: Ord a => Dist a -> Prob
@@ -74,7 +74,7 @@ bayesVuln = maximum . elems . runD . reduction
 -- | Based on an entropy function for distributions, calculate the
 -- average entropy for a hyper-distribution.
 condEntropy :: (Dist a -> Rational) -> Hyper a -> Rational
-condEntropy e m = average (fmap e m)
+condEntropy e m = average (fmapDist e m)
 
 -- | Average a distribution of Rationals.
 average :: Dist Rational -> Rational
